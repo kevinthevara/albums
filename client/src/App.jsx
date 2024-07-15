ee67ca077fbe76a4c08c4806e5672277f67cbcf2
Kevin Thevara <kevinthevara@gmail.com>
2024-07-15 18:59:39 -0400

    bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import { Route, Routes } from 'react-router-dom';
-// import { Home } from "./pages/home/Home";
-// import { Login } from "./pages/login/Login";
-// import { Signup } from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
 
 function App() { 
   return (

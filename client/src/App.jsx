5bc970a004585ebd465ebed6313d28db0d9c4999
Kevin Thevara <kevinthevara@gmail.com>
2024-07-15 18:45:55 -0400

    implemented pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
-import { Home } from "./pages/home/Home";
-import { Login } from "./pages/login/Login";
-import { Signup } from "./pages/signup/Signup";
+// import { Route, Routes } from 'react-router-dom';
+// import { Home } from "./pages/home/Home";
+// import { Login } from "./pages/login/Login";
+// import { Signup } from "./pages/signup/Signup";
 
 function App() { 
   return (
-    <div className= 'p-4 h-screen flex items-center justify-center'>
-      <Routes>
-        <Route path='/' element={< Home /> } />
-        <Route path='/login' element={< Login /> } />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Login />
     </div>
   )
-}
+};
 
 export default App;
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, signout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 
-router.post('/logout', logout);
+router.post('/logout', signout);
 
 router.post('/signup', signup);
 

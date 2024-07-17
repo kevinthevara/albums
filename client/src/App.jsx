e22f504c00000c7a683a927043b8b63ccf2e6444
Kevin Thevara <kevinthevara@gmail.com>
2024-07-17 19:01:03 -0400

    adding css for signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Login from "./pages/login/Login";
+import Login from "./pages/login/login";
+import Signup from './pages/signup/signup';
 
 function App() { 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Signup />
     </div>
   )
 };
--- client/src/pages/signup/signup.jsx
+++ client/src/pages/signup/signup.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 border rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-black'>
+          Sign Up <span className='text-blue-500'>Albums</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Full name</span>
+            </label>
+            <input type='text' placeholder='Input name' className='w-full input input-bordered h-10'></input>
+          </div>
+
+          <div>
+            <label className='label'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input type='text' placeholder='Input username' className='w-full input input-bordered h-10'></input>
+          </div>
+
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input type='password' className='w-full input input-bordered h-10'></input>
+          </div>
+
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Confirm password</span>
+            </label>
+            <input type='password' className='w-full input input-bordered h-10'></input>
+          </div>
+
+          <a className='text-sm hover:underline hover:text-blue-600 mt-4 inline-block' href='#'>
+            Already have an account?
+          </a>
+
+          <div>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign up</button>
+          </div>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
